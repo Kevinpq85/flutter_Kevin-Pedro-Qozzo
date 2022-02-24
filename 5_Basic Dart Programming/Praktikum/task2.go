d8f6cfceac91ae3ae75eff80f7362f908b6e310f
Kevinpq85 <97868354+Kevinpq85@users.noreply.github.com>
2022-02-24 22:24:58 +0700

    Tugas Section 5

---
+++ 5_Basic Dart Programming/Praktikum/task2.go
@@ -0,0 +1,6 @@
+// void main(){
+// 	String nama1 = "Kevin";
+// 	String nama2 = "Pedro";
+// 	String nama3 = "Qozzo";
+// 	print('Hallo nama saya' " " + nama1 + " " + nama2 + " " + nama3);
+//   } 
