d04417c9a9c0871d1ca084de476bd93207db1750
Kevinpq85 <97868354+Kevinpq85@users.noreply.github.com>
2022-03-08 17:46:14 +0700

    Tugas big task 1

--- big_task_1/Praktikum/task_1.dart
+++ big_task_1/Praktikum/task_1.dart
@@ -1,11 +1,19 @@
-class Hewan {
-  String nama = "Kucing";
-  int berat = 20; 
+import 'dart:io';
+
+fungsi (int a) {
+  if (a.isEven) {
+    return true;
+  } else if (a.isOdd) {
+    return false;
+  }
 }
 
 void main() {
-  var h1 = Hewan();
+  stdout.write("Silahkan masukkan angka : ");
+  int a = int.parse(stdin.readLineSync()!);
+  bool b = fungsi(a);
+  print(b);
 
-  print(h1.nama);
-  print("Berat : ${h1.berat}");
 }
+
+  
--- big_task_1/Praktikum/task_2.dart
+++ big_task_1/Praktikum/task_2.dart
@@ -1,6 +1,20 @@
-class Mobil {
-  String kucing;
-  String anjing;
-  String tikus;
-  void
+import 'dart:io';
+
+azimuth () {
+  stdout.write("Masukkan angka : ");
+  int? angka = int.parse(stdin.readLineSync()!);
+  if(angka >= 180){
+    angka = angka - 180;
+    return angka;
+  } else if(angka <= 180) {
+    angka = angka + 180;
+    return angka;
+  } else if (angka <=0 || angka >= 360) {
+    angka = 0;
+    return angka;
+  }
+}
+
+void main() {
+print(azimuth());
 }
